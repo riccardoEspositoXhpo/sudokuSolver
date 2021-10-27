7bff63ce7960595ac25190f0324192dfc3f9da8c
Riccardo Esposito <espositoriccardo101@gmail.com>
2021-10-27 20:46:37 +0200

    syncing files

--- sudokuSolver/sudokuSolver.py
+++ sudokuSolver/sudokuSolver.py
@@ -5,22 +5,10 @@ import pandas as pd
 
 sys.setrecursionlimit(10000)
 
-
-# grid = [
-#     [5,3,0,0,7,0,0,0,0],
-#     [6,0,0,1,9,5,0,0,0],
-#     [0,9,8,0,0,0,0,6,0],
-#     [8,0,0,0,6,0,0,0,3],
-#     [4,0,0,8,0,3,0,0,1],
-#     [7,0,0,0,2,0,0,0,6],
-#     [0,6,0,0,0,0,2,8,0],
-#     [0,0,0,4,1,9,0,0,5],
-#     [0,0,0,0,8,0,0,7,9]
-# ]
-
 grids = []
 solvedGrids = []
 solutions = 0
+
 
 # define global variables
 GRID_SIZE = 9
@@ -30,21 +18,33 @@ BOX_SIZE = 3
 def main():
     # print initial puzzle state
     
-    importPuzzles()
+    startTime = time.time()
 
-    # print(grids[0])
-    for i in range(len(grids)):
+    fileName = "sudokuMini.csv"
 
-        solvePuzzle(grids[i],solvedGrids[i])
 
-    print(solutions)                
-        
-def importPuzzles():
+    importPuzzles(fileName)
+
+    for i in  range(len(grids)):
+
+        # I want to limit the size of the problem first
+        # I the puzzleCleaner
+        #then I pass the result to BruteSovle
+        puzzleCleaner(i)
+
+        bruteSolve(i)
+
+    print(solutions)
+    print(len(grids))                
+    print("Time Elapsed: " + str(time.time() - startTime) + " seconds")
+
+
+def importPuzzles(fileName):
     
     global grids
     global solvedGrids
 
-    with open("sudokuSolver/sudokuTiny.csv", "r") as file: 
+    with open("sudokuSolver/" + fileName, "r") as file: 
         reader = pd.read_csv(file)
         for index,row in reader.iterrows():
             
@@ -65,16 +65,18 @@ def importPuzzles():
 
 
 # function receives y and x position in the grid and checks if number is allowed
-def possibleValues(grid, y, x, n):
+def possibleValues(idx, y, x, n):
     
+    global grids
+
     # check 1 - does n exist on same row
     for i in range(GRID_SIZE):
-        if grid[y][i] == n:
+        if grids[idx][y][i] == n:
             return False
     
     # check 2 - does n exist on same column
     for i in range(GRID_SIZE):
-        if grid[i][x] == n:
+        if grids[idx][i][x] == n:
             return False
     
     # check 3 - does n exist in the same box
@@ -83,40 +85,70 @@ def possibleValues(grid, y, x, n):
     
     for i in range(BOX_SIZE):
         for j in range(BOX_SIZE):
-            if grid[rowStart + i][colStart + j] == n:
+            if grids[idx][rowStart + i][colStart + j] == n:
                 return False
     
     # n is a valid candidate
     return True
 
 
-def solvePuzzle(grid, solved):  
+def bruteSolve(i):  
 
     global grids
+    global solvedGrids
+    global options
     global solutions
+    options = []
 
     for y in range(GRID_SIZE):
         for x in range(GRID_SIZE):
-            if grid[y][x] == 0:
+            if grids[i][y][x] == 0:
                 for n in range(1, GRID_SIZE + 1):
 
                     # find if a number can be inserted
-                    if possibleValues(grid, y, x, n):
-
+                    if possibleValues(i, y, x, n):
+                            
                         # assign number to grid
-                        grid[y][x] = n
+                        grids[i][y][x] = n
 
                         # recursively call solve to finish puzzle
-                        solvePuzzle(grid, solved)
+                        bruteSolve(i)
 
                         # if the above returns we backtrack and set it to 0
-                        grid[y][x] = 0
+                        grids[i][y][x] = 0
 
                 return 
 
-    if np.array_equiv(grid,solved):
+    if np.array_equiv(grids[i],solvedGrids[i]):
         solutions+= 1
-        print(solutions)
+        print(i+1, solutions)
+    
+    else: 
+        print("puzzle ->" + str(i) + " was not solved!")
+
+
+def puzzleCleaner(i):
+
+    global grids
+    # global options
+
+    for y in range(GRID_SIZE):
+        for x in range(GRID_SIZE):
+            if grids[i][y][x] == 0:
+                options = []
+                for n in range(1, GRID_SIZE + 1):
+                    if possibleValues(i, y, x, n):
+                        
+                        options.append(n)
+
+                if len(options) == 1:
+                    
+                    grids[i][y][x] = options[0]
+                    
+                    # recursively call function to find unique matches
+                    puzzleCleaner(i)
+    
+    return
 
 
 if __name__ == "__main__":
